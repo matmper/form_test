f9910d6ae398fb1210908f4afca3b081d0117d4c
JoaoPMV <joaop.trilha@gmail.com>
2024-12-07 19:20:07 -0300

    first commit

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,19 +1,23 @@
+
+
 const UsuarioController = {
 
 login: (req,res) => {
   res.render('index')
 },
 
-save: (req,res) => {
-  Puppy.create({
-    name: req.body.name,
-    sex: req.body.sex,
-    age: req.body.age,
-    gender: req.body.gender,
-    size: req.body.size,
-  })
- res.redirect('/')
+save: (req, res) => {
+  try {
+      // Desestruturando os dados do req.body
+      const { name, gender, color, size, age } = req.body;
+       Puppy.create({ name, gender, color, size, age });
+      res.redirect('/');
+  } catch (error) {
+      console.error("Erro ao criar Puppy:", error.message);
+      res.status(500).send('Erro ao salvar os dados. Tente novamente.');
+  }
 }
+
 }
 
 module.exports = UsuarioController
